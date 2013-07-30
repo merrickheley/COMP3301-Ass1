776b093a4758b348ac20cd33827aa99d057b197c
merrick.heley <masterheley@hotmail.com>
2013-07-31 01:26:05 +1000

    removed doxygen commenting, wrote initial cwd prompt

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 /**
- *  \file main.c
- *  \author Merrick Heley (merrick.heley@uqconnect.edu.au)
- *  \brief Simple shell replacement
- *
- *  \details
+ *  main.c
+ *  Merrick Heley (merrick.heley@uqconnect.edu.au)
+ *  Simple shell replacement
  *
  *  qshell is a simple shell written in C.
  *
- *  COMP3301 Assignment 1 \n
+ *  COMP3301 Assignment 1
  *  Copyright (C) 2013  Merrick Heley
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -22,19 +20,67 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http: *www.gnu.org/licenses/>.
- *
-
  */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-/* Exit Statuses */
-#define ERR_SUCCESS    0
+#define MAXCHARS 150
 
-int main (int argc, char **argv) {
+#define ERR_SUCCESS 0
+#define ERR_CWD 1
 
-    printf("Started!\r\n");
+/**
+ * prints the prompt to the user for a command
+ */
+void print_prompt(char *cwd) {
+    if (getcwd(cwd, MAXCHARS) == NULL) {
+        perror("getcwd() error");
+        exit(ERR_CWD);
+    }
 
-    return ERR_SUCCESS;
+    printf("%s? ", cwd);
+}
+
+/**
+ * main execution loop
+ */
+int main(int argc, char **argv) {
+    char buf[MAXCHARS];
+    char cwd[MAXCHARS];
+    pid_t pid;
+    int status;
+
+    /* print initial prompt */
+    print_prompt(cwd);
+
+    while (fgets(buf, MAXCHARS, stdin) != NULL) {
+
+        /* replace newline with null */
+        buf[strlen(buf) - 1] = 0;
+
+        pid = fork();
+        if (pid < 0) {
+            printf("fork error");
+        }
+        /* child */
+        else if (pid == 0) {
+            execlp(buf, buf, (char *) 0);
+            printf("couldn't execute: %s \r\n", buf);
+            return (127);
+        }
+        /* parent */
+        if ((pid = waitpid(pid, &status, 0)) < 0) {
+            printf("waitpid error");
+        }
+
+        /* print prompt */
+        print_prompt(cwd);
+    }
+
+    return (ERR_SUCCESS);
 }
